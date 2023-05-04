62e0383c210da6939a3c9c438723b57dcdf0bdd2
Lautaro <lautaroencalado@gmail.com>
2023-05-04 13:55:43 -0300

    "primer index"

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
-//src identifica archivo  y dest la guarda en una carpeta destino
-const { src, dest, watch } =  require("gulp");
-const sass =  require("gulp-sass") (require('sass'));
+const { src, dest, watch } = require('gulp'); 
+const sass =  require('gulp-sass')(require('sass')); 
 
-function  css (callbak) {
-    src('src/scss/**/*.scss')    //Identificar archivo SASS
-        .pipe(sass() )              //Compilarlo, para hacerlo usamos el pipe()    
-        .pipe(dest('/build/css'));  //Almacenarlo en el disco duro
-    callbak();
-}
+function css(done) {     
+    src('src/scss/**/*.scss')   // identificar el archivos de SASS         
+        .pipe(sass()) // Compilarlo         
+        .pipe(dest("build/css"))  // Almacenar en el disco duro     
+    done(); // callback que avisa a gulp cuando lleguemos al final 
+} 
 
-function dev(callbak) {
-    watch('src/scss/**/*.scss', css)
+function dev(done) {     
+    watch("src/scss/**/*.scss", css)             
+    done(); 
+} 
 
-    callbak()
-}
-exports.css = css;  
-exports.dev = dev;  
+exports.css = css; 
+exports.dev = dev;
